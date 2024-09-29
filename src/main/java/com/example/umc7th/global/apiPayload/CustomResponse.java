66b939ed9dfac396f879142edb62150df4edeafa
suminiee <chansu0114@naver.com>
2024-09-29 18:17:33 +0900

    ✨ [Feat] Swagger 설정 및 응답 통일

--- src/main/java/com/example/umc7th/global/apiPayload/CustomResponse.java
+++ src/main/java/com/example/umc7th/global/apiPayload/CustomResponse.java
@@ -1,5 +1,7 @@
 package com.example.umc7th.global.apiPayload;
 
+import com.example.umc7th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc7th.global.apiPayload.code.BaseSuccessCode;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AccessLevel;
@@ -9,8 +11,8 @@ import org.springframework.http.HttpStatus;
 //생성자로 객체를 생성하는 것을 방지
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 //json 형식으로 줄 때 어떤 순서로, 어떤 변수들을 줄것인지 결정하는 annotation
-@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
-public class CustomResponse {
+@JsonPropertyOrder({"isSuccess", "status", "code", "message", "result"})
+public class CustomResponse<T> {
 
     @JsonProperty("isSuccess") // isSuccess라는 변수라는 것을 명시하는 Annotation
     private boolean isSuccess;
@@ -24,7 +26,14 @@ public class CustomResponse {
     @JsonProperty("message")
     private String message;
 
-    public static CustomResponse onSuccess() {
-        return new CustomResponse(true, HttpStatus.OK, String.valueOf(HttpStatus.OK.value()), HttpStatus.OK.getReasonPhrase());
+    @JsonProperty("result")
+    private T result;
+
+    public static <T> CustomResponse<T> onSuccess(BaseSuccessCode code, T result) {
+        return new CustomResponse<>(true, code.getStatus(), code.getCode(), code.getMessage(), result);
+    }
+
+    public static <T> CustomResponse<T> onFailure(BaseErrorCode code, T result) {
+        return new CustomResponse<>(false, code.getStatus(), code.getCode(), code.getMessage(), result);
     }
 }
