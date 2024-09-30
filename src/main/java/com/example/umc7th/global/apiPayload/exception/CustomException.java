e1cee62e9da5b5f3ae425e1d3531ebb435ee8389
Jeongmo Seo <wjdah1402@gmail.com>
2024-09-30 20:58:43 +0900

    :sparkles: feat: 응답 통일 및 에러 핸들러 구현

---
+++ src/main/java/com/example/umc7th/global/apiPayload/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.example.umc7th.global.apiPayload.exception;
+
+import com.example.umc7th.global.apiPayload.code.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public CustomException(BaseErrorCode code) {
+        this.code = code;
+    }
+
+}
