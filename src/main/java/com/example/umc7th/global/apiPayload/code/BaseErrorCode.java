9feeb8f73cc059ea0a445485277c7ef348351cea
RCNR <iop04226@gmail.com>
2024-10-01 14:07:34 +0900

    :sparkles: 응답 통일 getResponse() 추가

--- src/main/java/com/example/umc7th/global/apiPayload/code/BaseErrorCode.java
+++ src/main/java/com/example/umc7th/global/apiPayload/code/BaseErrorCode.java
@@ -1,5 +1,6 @@
 package com.example.umc7th.global.apiPayload.code;
 
+import com.example.umc7th.global.apiPayload.CustomResponse;
 import org.springframework.http.HttpStatus;
 
 // 다른 에러들을 추상화 할 인터페이스 생성
@@ -10,4 +11,7 @@ public interface BaseErrorCode {
     String getCode();
 
     String getMessage();
+
+
+    <T> CustomResponse<T> getResponse();
 }
