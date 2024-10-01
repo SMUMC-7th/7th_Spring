00d609d07e7ea7f8d8d7ded361ee327ccb23a141
RCNR <iop04226@gmail.com>
2024-10-01 14:16:58 +0900

    :sparkles: 실패 메소드 추가

--- src/main/java/com/example/umc7th/domain/test/TestController.java
+++ src/main/java/com/example/umc7th/domain/test/TestController.java
@@ -1,8 +1,12 @@
 package com.example.umc7th.domain.test;
 
+import com.example.umc7th.global.apiPayload.CustomResponse;
+import com.example.umc7th.global.apiPayload.code.GeneralErrorCode;
+import com.example.umc7th.global.apiPayload.exception.CustomException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +15,17 @@ public class TestController {
 
     @GetMapping("/test")
     @Operation(summary = "테스트하는 API", description = "테스트용도이며 Hello World를 반환합니다.")
-    public String test() {
-        return "Hello World";
+    public CustomResponse<String> test() {
+        return CustomResponse.onSuccess("Hello World");
+    }
+
+    @GetMapping("/test/failure")
+    public CustomResponse<String> failure(@RequestParam int exception) {
+        if (exception == 0) {
+            throw new CustomException(GeneralErrorCode.BAD_REQUEST_400);
+        } else if (exception == 1) {
+            int a = 1 / 0;
+        }
+        return CustomResponse.onSuccess("에러 핸들러 동작하지 않음");
     }
 }
