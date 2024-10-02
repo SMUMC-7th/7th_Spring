82078c464953fee9835b3be80c6615c5bf9062fb
Jimyeong Kim <kjm021221@icloud.com>
2024-10-02 12:52:19 +0900

    ✨feat: 도메인 Entity 및 BaseEntity 작성

---
+++ src/main/java/com/example/umc7th/domain/article/entity/Article.java
@@ -0,0 +1,32 @@
+package com.example.umc7th.domain.article.entity;
+
+import com.example.umc7th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+@Table(name = "article")
+public class Article extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "like_num")
+    private int likeNum;
+
+    // Reply과의 양방향 매핑은 필요하다 생각 될 때 걸어주겠습니다.
+
+}
