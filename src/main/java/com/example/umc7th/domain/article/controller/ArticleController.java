432df34c3dff827f06a537cd8cfae73527da9a50
Jimyeong Kim <kjm021221@icloud.com>
2024-10-04 16:19:38 +0900

    ✨feat: Swagger 설명 추가

--- src/main/java/com/example/umc7th/domain/article/controller/ArticleController.java
+++ src/main/java/com/example/umc7th/domain/article/controller/ArticleController.java
@@ -5,6 +5,8 @@ import com.example.umc7th.domain.article.dto.response.ArticleResDto;
 import com.example.umc7th.domain.article.service.command.ArticleCommandService;
 import com.example.umc7th.domain.article.service.query.ArticleQueryService;
 import com.example.umc7th.global.apiPayload.CustomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,11 +19,13 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/articles")
+@Tag(name = "게시글 관련 API", description = "게시글 관련 API입니다.")
 public class ArticleController {
 
     private final ArticleCommandService articleCommandService;
     private final ArticleQueryService articleQueryService;
 
+    @Operation(summary = "게시글 작성", description = "request로 넘긴 title과 content로 게시글을 생성합니다.")
     @PostMapping("")
     public ResponseEntity<CustomResponse<ArticleResDto.CreateArticleResponseDto>> createArticle(
             @RequestBody ArticleReqDto.CreateArticleRequestDto requestDto) {
@@ -32,12 +36,14 @@ public class ArticleController {
                 .body(CustomResponse.onSuccess(HttpStatus.CREATED, responseDto));
     }
 
+    @Operation(summary = "전체 게시글 리스트 조회", description = "전체 게시글을 조회합니다.")
     @GetMapping("")
     public CustomResponse<List<ArticleResDto.CreateArticleResponseDto>> getArticleList() {
         List<ArticleResDto.CreateArticleResponseDto> articles = articleQueryService.getArticleList();
         return CustomResponse.onSuccess(articles);
     }
 
+    @Operation(summary = "게시글 단일 조회", description = "게시글의 ID로 게시글을 단일 조회합니다.")
     @GetMapping("/{articleId}")
     public CustomResponse<ArticleResDto.CreateArticleResponseDto> getArticle(@PathVariable Long articleId) {
         ArticleResDto.CreateArticleResponseDto responseDto = articleQueryService.getArticle(articleId);
--- src/main/java/com/example/umc7th/domain/reply/controller/ReplyController.java
+++ src/main/java/com/example/umc7th/domain/reply/controller/ReplyController.java
@@ -5,6 +5,8 @@ import com.example.umc7th.domain.reply.dto.response.ReplyResDto;
 import com.example.umc7th.domain.reply.service.command.ReplyCommandService;
 import com.example.umc7th.domain.reply.service.query.ReplyQueryService;
 import com.example.umc7th.global.apiPayload.CustomResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,12 +19,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/replies")
+@Tag(name = "댓글 API", description = "댓글 관련 API입니다.")
 public class ReplyController {
 
     private final ReplyCommandService replyCommandService;
     private final ReplyQueryService replyQueryService;
 
     // 댓글 생성
+    @Operation(summary = "댓글 생성", description = "request로 넘긴 articleId로 조회한 게시글에 댓글을 생성합니다.")
     @PostMapping("")
     public ResponseEntity<CustomResponse<ReplyResDto.CreateReplyResponseDto>> createReply(
             @RequestBody ReplyReqDto.CreateReplyRequestDto requestDto) {
@@ -34,6 +38,7 @@ public class ReplyController {
     }
 
     // 게시글 별 댓글 조회
+    @Operation(summary = "댓글 생성", description = "URL로 넘긴 articleId로 조회한 게시글의 댓글을 전체 조회합니다.")
     @GetMapping("/article/{articleId}")
     public CustomResponse<List<ReplyResDto.CreateReplyResponseDto>> getRepliesByArticleId(
             @PathVariable Long articleId) {
