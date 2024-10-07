d7171730b4d335af508ae50401eb873998f6dab9
CHO <imalex12@naver.com>
2024-10-08 01:49:13 +0900

    delete : 이전 pr로 인한 신규 작성사항까지 merge됨 때문에 신규pr을 위해 변경사항삭제

--- src/main/java/com/example/umc7th/domain/article/service/command/ArticleCommandService.java
+++
@@ -1,8 +0,0 @@
-package com.example.umc7th.domain.article.service.command;
-
-import com.example.umc7th.domain.article.dto.ArticleRequestDTO;
-import com.example.umc7th.domain.article.entity.Article;
-
-public interface ArticleCommandService {
-    Article createArticle(ArticleRequestDTO.CreateArticleDTO dto);
-}
