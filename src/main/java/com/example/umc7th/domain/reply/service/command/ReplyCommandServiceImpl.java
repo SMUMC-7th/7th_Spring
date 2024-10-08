b3152bb72484874dc851e6975b8634c60353a59b
Jeongmo Seo <wjdah1402@gmail.com>
2024-10-08 19:58:43 +0900

    :sparkles: feat: 4주차 스터디 초안

--- src/main/java/com/example/umc7th/domain/reply/service/command/ReplyCommandServiceImpl.java
+++ src/main/java/com/example/umc7th/domain/reply/service/command/ReplyCommandServiceImpl.java
@@ -7,6 +7,8 @@ import com.example.umc7th.domain.article.repository.ArticleRepository;
 import com.example.umc7th.domain.reply.converter.ReplyConverter;
 import com.example.umc7th.domain.reply.dto.ReplyRequestDTO;
 import com.example.umc7th.domain.reply.entity.Reply;
+import com.example.umc7th.domain.reply.exception.ReplyErrorCode;
+import com.example.umc7th.domain.reply.exception.ReplyException;
 import com.example.umc7th.domain.reply.repository.ReplyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,5 +27,21 @@ public class ReplyCommandServiceImpl implements ReplyCommandService {
         Article article = articleRepository.findById(dto.getArticleId()).orElseThrow(() ->
                 new ArticleException(ArticleErrorCode.NOT_FOUND));
         return replyRepository.save(ReplyConverter.toReply(dto, article));
+    }
+
+    @Override
+    public Reply updateReply(Long id, ReplyRequestDTO.UpdateReplyDTO dto) {
+        Reply reply = replyRepository.findById(id).orElseThrow(() ->
+                new ReplyException(ReplyErrorCode.NOT_FOUND));
+        reply.update(dto.getContent());
+        return reply;
+    }
+
+    @Override
+    public Long deleteReply(Long id) {
+        Reply reply = replyRepository.findById(id).orElseThrow(() ->
+                new ReplyException(ReplyErrorCode.NOT_FOUND));
+        replyRepository.delete(reply);
+        return id;
     }
 }
