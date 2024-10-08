71bd623e9f024116bea35f76f5c56e378ab11b07
Jeongmo Seo <wjdah1402@gmail.com>
2024-10-08 19:57:05 +0900

    :bug: fix: 게시글 api 수정 및 yml 파일 수정

--- src/main/java/com/example/umc7th/domain/article/controller/ArticleController.java
+++ src/main/java/com/example/umc7th/domain/article/controller/ArticleController.java
@@ -21,21 +21,21 @@ public class ArticleController {
     private final ArticleQueryService articleQueryService;
     private final ArticleCommandService articleCommandService;
 
-    @PostMapping("/article")
+    @PostMapping("/articles")
     @Operation(summary = "게시글 생성 API", description = "게시글 생성하는 API")
     public CustomResponse<ArticleResponseDTO.CreateArticleResponseDTO> createArticle(@RequestBody ArticleRequestDTO.CreateArticleDTO dto) {
         Article article = articleCommandService.createArticle(dto);
         return CustomResponse.onSuccess(ArticleResponseDTO.CreateArticleResponseDTO.from(article));
     }
 
-    @GetMapping("/article/{articleId}")
+    @GetMapping("/articles/{articleId}")
     @Operation(summary = "게시글 조회 API", description = "게시글 하나 조회하는 API")
     public CustomResponse<ArticleResponseDTO.ArticlePreviewDTO> getArticle(@PathVariable("articleId") Long articleId) {
         Article article = articleQueryService.getArticle(articleId);
         return CustomResponse.onSuccess(ArticleResponseDTO.ArticlePreviewDTO.from(article));
     }
 
-    @GetMapping("/article")
+    @GetMapping("/articles")
     @Operation(summary = "게시글 전체 조회 API", description = "게시글 전체 조회하는 API")
     public CustomResponse<ArticleResponseDTO.ArticlePreviewListDTO> getArticles() {
         List<Article> articles = articleQueryService.getArticles();
