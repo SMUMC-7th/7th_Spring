f875f70342b7552b259700a25920864183e65d7f
leesj <jinycap7@gmail.com>
2024-10-08 19:36:09 +0900

    ✨ [Feat] Article, Reply CRUD 중 CR 구현

--- src/main/java/com/example/umc7th/domain/reply/service/query/ReplyQueryServiceImpl.java
+++ src/main/java/com/example/umc7th/domain/reply/service/query/ReplyQueryServiceImpl.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReplyQueryServiceImpl implements ReplyQueryService {
 
