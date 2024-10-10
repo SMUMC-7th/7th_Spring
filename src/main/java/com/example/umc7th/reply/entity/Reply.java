959254391ed07fcedcbc3589116ac3f7a7cdc9ce
chaerish <ccyy8432@naver.com>
2024-10-11 00:08:21 +0900

    :sparkles: feat: Reply UD 기능 추가

--- src/main/java/com/example/umc7th/reply/entity/Reply.java
+++ src/main/java/com/example/umc7th/reply/entity/Reply.java
@@ -4,12 +4,14 @@ import com.example.umc7th.article.entity.Article;
 import com.example.umc7th.article.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.Where;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE) //필더 패턴으로 제한한다.
 @Builder
 @Getter
+@Where(clause = "is_deleted = false")
 public class Reply extends BaseTimeEntity {
     @Id
     // PK의 생성 전략 설정
@@ -20,4 +22,8 @@ public class Reply extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "article_id")
     private Article article;
+
+    public void update(String content) {
+        this.content = content;
+    }
 }
