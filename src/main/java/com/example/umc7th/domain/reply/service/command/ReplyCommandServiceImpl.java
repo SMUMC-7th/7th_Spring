3a93bc373e8ddbf8e3eabe606e9126044ff00932
RCNR <iop04226@gmail.com>
2024-10-11 18:34:24 +0900

    :sparkles: feat: update, delete 서비스 메소드 추가

--- src/main/java/com/example/umc7th/domain/reply/service/command/ReplyCommandServiceImpl.java
+++ src/main/java/com/example/umc7th/domain/reply/service/command/ReplyCommandServiceImpl.java
@@ -5,9 +5,13 @@ import com.example.umc7th.domain.article.exception.ArticleErrorCode;
 import com.example.umc7th.domain.article.exception.ArticleException;
 import com.example.umc7th.domain.article.repository.ArticleRepository;
 import com.example.umc7th.domain.reply.converter.ReplyConverter;
+import com.example.umc7th.domain.reply.dto.ReplyResponseDTO;
+import com.example.umc7th.domain.reply.exception.ReplyErrorCode;
+import com.example.umc7th.domain.reply.exception.ReplyException;
 import com.example.umc7th.domain.reply.repository.ReplyRepository;
 import com.example.umc7th.domain.reply.dto.ReplyRequestDTO;
 import com.example.umc7th.domain.reply.entity.Reply;
+import com.example.umc7th.global.apiPayload.CustomResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,4 +32,25 @@ public class ReplyCommandServiceImpl implements ReplyCommandService {
 
         return replyRepository.save(ReplyConverter.toReply(dto, article));
     }
+
+    @Override
+    public ReplyResponseDTO.UpdateReplyResponseDTO updateReply(Long id, ReplyRequestDTO.UpdateReplyDTO dto) {
+
+        Reply reply = replyRepository.findById(id).orElseThrow(() ->
+                new ReplyException(ReplyErrorCode.NOT_FOUND)); // 영속 상태 엔티티 가져옴
+
+        reply.update(dto); // 영속 상태 엔티티 내용 변경
+        return ReplyConverter.toUpdateReplyResponseDTO(reply);
+    }
+
+    @Override
+    public void deleteReply(Long id) {
+
+        replyRepository.findById(id).orElseThrow(() ->
+                new ReplyException(ReplyErrorCode.NOT_FOUND));
+
+        replyRepository.deleteById(id);
+    }
+
+
 }
