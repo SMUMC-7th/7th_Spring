ae8ea18b6e7177032cd0378940299d1811bc4d6a
chaerish <ccyy8432@naver.com>
2024-10-29 14:47:26 +0900

    🐛bug(ReplyController): swagger description 수정

--- src/main/java/com/example/umc7th/reply/controller/ReplyController.java
+++ src/main/java/com/example/umc7th/reply/controller/ReplyController.java
@@ -9,7 +9,13 @@ import com.example.umc7th.reply.service.query.ReplyQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,15 +27,17 @@ public class ReplyController {
     //Create, Post 요청 사용
     @PostMapping("/{articleId}/replies")
     @Operation(summary = "reply 생성 API", description = "새 답글을 생성하는 API이고, 생성된 reply의 id를 반환합니다.")
-    public CustomResponse<?> createReply(@PathVariable Long articleId, @RequestBody ReplyRequestDTO.CreateReplyDTO dto) {
+    public CustomResponse<?> createReply(@PathVariable Long articleId,
+                                         @RequestBody ReplyRequestDTO.CreateReplyDTO dto) {
         return CustomResponse.onSuccess(GeneralSuccessCode.OK, replyCommandService.createReply(articleId, dto).getId());
     }
 
     //여러개 조회
     @GetMapping("/{articleId}/replies")
-    @Operation(summary = "게시글에 댓글 생성 API ", description = "게시글에 있는 답글을 조회하는 API입니다. ")
+    @Operation(summary = "게시글에 댓글 조회 API ", description = "게시글에 있는 답글을 조회하는 API입니다. ")
     public CustomResponse<?> getAllReplies(@PathVariable("articleId") Long articleId) {
-        return CustomResponse.onSuccess(GeneralSuccessCode.OK, ReplyResponseDTO.from(replyQueryService.getReplies(articleId)));
+        return CustomResponse.onSuccess(GeneralSuccessCode.OK,
+                ReplyResponseDTO.from(replyQueryService.getReplies(articleId)));
     }
 
     @PutMapping("/{replyId}/replies")
