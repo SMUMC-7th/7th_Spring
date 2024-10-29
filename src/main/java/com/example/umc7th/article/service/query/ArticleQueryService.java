d08171233b56bd6d1fc9a279f83795d1f51ee107
chaerish <ccyy8432@naver.com>
2024-10-29 13:17:09 +0900

    ✨feat(ArticleService): Article Repository title 검색 기능과 커서 기반 페이지네이션 기능 추가

--- src/main/java/com/example/umc7th/article/service/query/ArticleQueryService.java
+++ src/main/java/com/example/umc7th/article/service/query/ArticleQueryService.java
@@ -1,14 +1,13 @@
 package com.example.umc7th.article.service.query;
 
 import com.example.umc7th.article.entity.Article;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 //조회는 Query
 public interface ArticleQueryService {
     Article getDetailArticle(Long id);
 
-    List<Article> getArticles();
+    List<Article> getArticles(Long cursorId, String likeTitle);
 }
