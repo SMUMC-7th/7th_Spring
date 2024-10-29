a729a41595ad815856b7ef83d40ee3f98a7130ec
chaerish <ccyy8432@naver.com>
2024-10-29 12:25:59 +0900

    ✨feat(ArticleRepository): id로 게시글 Cursor 기반 페이지네이션 구현
    ID로 게시글 커서 기반 페이지네이션을 구현한다.

--- src/main/java/com/example/umc7th/article/repository/ArticleRepository.java
+++ src/main/java/com/example/umc7th/article/repository/ArticleRepository.java
@@ -1,10 +1,14 @@
 package com.example.umc7th.article.repository;
 
 import com.example.umc7th.article.entity.Article;
+import java.awt.print.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     boolean existsById(Long id);
+
+    Slice<Article> findAllById(Long id, Pageable pageable);
 }
