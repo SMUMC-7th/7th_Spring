bdeab94d92a2e45ea8609d0201e0587a779eed2a
chaerish <ccyy8432@naver.com>
2024-10-29 14:48:27 +0900

    🐛refactor(ReplyRepository): JPQL 메서드 이름 수정

--- src/main/java/com/example/umc7th/reply/repository/ReplyRepository.java
+++ src/main/java/com/example/umc7th/reply/repository/ReplyRepository.java
@@ -2,9 +2,9 @@ package com.example.umc7th.reply.repository;
 
 import com.example.umc7th.article.entity.Article;
 import com.example.umc7th.reply.entity.Reply;
-import java.awt.print.Pageable;
 import java.util.List;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,5 +17,5 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     boolean existsByArticleId(Long articleId);
 
     @Query("SELECT r from Reply r WHERE r.article =:article order by r.createdAt DESC ")
-    Page<Reply> findAllByArticleIdIsOrderByCreatedAtDesc(@Param("article") Article article, Pageable pageable);
+    Page<Reply> findAllByArticleIdByJPQL(@Param("article") Article article, Pageable pageable);
 }
