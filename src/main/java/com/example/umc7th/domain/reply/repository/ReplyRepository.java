7d58660b6fdc4f7b99a69256c02c83f832634d39
leeeemj <leemj2944@gmail.com>
2024-10-29 11:16:33 +0900

    :sparkles: feat : 게시글 댓글 확인 쿼리 생성

--- src/main/java/com/example/umc7th/domain/reply/repository/ReplyRepository.java
+++ src/main/java/com/example/umc7th/domain/reply/repository/ReplyRepository.java
@@ -4,12 +4,19 @@ import com.example.umc7th.domain.article.entity.Article;
 import com.example.umc7th.domain.reply.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 //    List<Reply> findByArticle(Article article);
     // active상태인 댓글만 조회되도록(softDelete)
-    @Query("SELECT r FROM Reply r WHERE r.active = true")
-    List<Reply> findAllNotDeleted();
+
+    //Method 해당 article을 가지고 있는 모든 답글 검색(생성날짜 내림차순 정렬)
+    //JPA Query
+    //List<Reply> findAllByArticleIsOrderByCreatedAtDesc(Article article);
+
+    //JPQL ver.
+    @Query("SELECT r FROM Reply r WHERE r.article = :article ORDER BY r.createdAt DESC")
+    List<Reply> findRepliesByArticleOrderByCreatedAtDescJPQL(@Param("article") Article article);
 }
