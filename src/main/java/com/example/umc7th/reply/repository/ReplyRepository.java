6830a4b06effdb28c441d2ddae43e795ba4e1206
chaerish <ccyy8432@naver.com>
2024-10-29 12:16:42 +0900

    ✨feat(ReplyRepository): 댓글 확인하는 Query 생성 및 댓글 페이지네이션 구현
    게시글에 댓글이 있는지 확인하는 Query를 생성하고, offset기반 페이지 네이션을 구현한다.

--- src/main/java/com/example/umc7th/reply/repository/ReplyRepository.java
+++ src/main/java/com/example/umc7th/reply/repository/ReplyRepository.java
@@ -1,12 +1,21 @@
 package com.example.umc7th.reply.repository;
 
+import com.example.umc7th.article.entity.Article;
 import com.example.umc7th.reply.entity.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
+import java.awt.print.Pageable;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
     List<Reply> findAllByArticleId(Long articleId);
+
+    boolean existsByArticleId(Long articleId);
+
+    @Query("SELECT r from Reply r WHERE r.article =:article order by r.createdAt DESC ")
+    Page<Reply> findAllByArticleIdIsOrderByCreatedAtDesc(@Param("article") Article article, Pageable pageable);
 }
