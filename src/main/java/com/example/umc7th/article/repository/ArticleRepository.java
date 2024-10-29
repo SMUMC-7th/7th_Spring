71d2412e3b5bd71d3473a2f2157cb76a1588554d
chaerish <ccyy8432@naver.com>
2024-10-29 13:16:49 +0900

    ✨feat(ArticleRepository): Article Repository title 검색 추가

--- src/main/java/com/example/umc7th/article/repository/ArticleRepository.java
+++ src/main/java/com/example/umc7th/article/repository/ArticleRepository.java
@@ -1,7 +1,7 @@
 package com.example.umc7th.article.repository;
 
 import com.example.umc7th.article.entity.Article;
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     boolean existsById(Long id);
 
-    Slice<Article> findAllById(Long id, Pageable pageable);
+    Slice<Article> findByIdLessThanAndTitleContainingOrderByIdDesc(Long id, Pageable pageable, String title);
+
+    Slice<Article> findByIdLessThanOrderByIdDesc(Long id, Pageable pageable);
 }
