730e265e34eb0ced8829a715d5b70cbd0ed35f86
chaerish <ccyy8432@naver.com>
2024-10-29 13:17:09 +0900

    ✨feat(ArticleService): Article Repository title 검색 기능과 커서 기반 페이지네이션 기능 추가

--- src/main/java/com/example/umc7th/article/service/query/ArticleQueryServiceImpl.java
+++ src/main/java/com/example/umc7th/article/service/query/ArticleQueryServiceImpl.java
@@ -4,11 +4,13 @@ import com.example.umc7th.article.entity.Article;
 import com.example.umc7th.article.repository.ArticleRepository;
 import com.example.umc7th.global.apiPayload.code.GeneralErrorCode;
 import com.example.umc7th.global.apiPayload.exception.CustomException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
@@ -24,7 +26,24 @@ public class ArticleQueryServiceImpl implements ArticleQueryService {
     }
 
     @Override
-    public List<Article> getArticles() {
-        return articleRepository.findAll();
+    public List<Article> getArticles(Long cursorId, String likeTitle) {
+        Pageable pageable = PageRequest.of(0, 10);
+        Slice<Article> slice = checkLikeTitle(cursorId, likeTitle, pageable);
+        List<Article> articles = slice.getContent();
+        while (slice.hasNext()) {
+            slice = checkLikeTitle(cursorId, likeTitle, slice.nextPageable());
+            System.out.println("Articles found: " + slice.getContent().size());
+            articles.addAll(slice.getContent());
+        }
+        return articles;
+
+    }
+
+    private Slice<Article> checkLikeTitle(Long cursorId, String likeTitle, Pageable pageable) {
+        if (likeTitle != null && !likeTitle.trim().isEmpty()) {
+            return articleRepository.findByIdLessThanAndTitleContainingOrderByIdDesc(cursorId,
+                    pageable, likeTitle);
+        }
+        return articleRepository.findByIdLessThanOrderByIdDesc(cursorId, pageable);
     }
 }
