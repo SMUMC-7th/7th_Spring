f32391e0d05f84d6c69f31ef0ad8905d88ad2435
chaerish <ccyy8432@naver.com>
2024-11-03 20:33:04 +0900

    refactor(ArticleService): article 좋아요 관련 에러 처리 추가

--- src/main/java/com/example/umc7th/reply/service/query/ReplyQueryServiceImpl.java
+++ src/main/java/com/example/umc7th/reply/service/query/ReplyQueryServiceImpl.java
@@ -1,15 +1,14 @@
 package com.example.umc7th.reply.service.query;
 
+import com.example.umc7th.article.error.ArticleCustomException;
+import com.example.umc7th.article.error.ArticleErrorCode;
 import com.example.umc7th.article.repository.ArticleRepository;
-import com.example.umc7th.global.apiPayload.code.GeneralErrorCode;
-import com.example.umc7th.global.apiPayload.exception.CustomException;
 import com.example.umc7th.reply.entity.Reply;
 import com.example.umc7th.reply.repository.ReplyRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
@@ -22,7 +21,7 @@ public class ReplyQueryServiceImpl implements ReplyQueryService {
     @Override
     public List<Reply> getReplies(Long articleId) {
         if (!articleRepository.existsById(articleId)) {
-            throw new CustomException(GeneralErrorCode.ARTICLE_NOT_FOUND_404);
+            throw new ArticleCustomException(ArticleErrorCode.ARTICLE_NOT_FOUND_404);
         }
         return replyRepository.findAllByArticleId(articleId);
     }
