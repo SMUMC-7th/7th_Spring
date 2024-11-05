97f4561ab873924131049b80adf8dc5ad1714824
chaerish <ccyy8432@naver.com>
2024-11-06 04:01:25 +0900

    feat: 스프링 시큐리티 관련 인증객체, config 설정 추가

--- src/main/java/com/example/umc7th/global/config/SecurityConfig.java
+++ src/main/java/com/example/umc7th/global/config/SecurityConfig.java
@@ -1,27 +1,39 @@
 package com.example.umc7th.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final BCryptPasswordEncoder encoder;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/swagger-ui/index.html", "/signup", "/login").permitAll()
+                        .requestMatchers("/swagger-ui/index.html", "/sign-up", "/login").permitAll()
                         .anyRequest().permitAll()
                 );
 
         return http.build();
 
     }
+
+    @Bean
+    public PasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
