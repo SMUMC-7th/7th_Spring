6d1d29ea308f2382336f0f21e2839d986c280d5d
leeeemj <leemj2944@gmail.com>
2024-11-12 14:27:39 +0900

    :sparkles: [Feat] 소셜로그인 컨트롤러 구현

--- src/main/java/com/example/umc7th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc7th/domain/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.example.umc7th.domain.member.controller;
 
 import com.example.umc7th.domain.member.dto.MemberRequestDTO;
 import com.example.umc7th.domain.member.dto.MemberResponseDTO;
+import com.example.umc7th.domain.member.service.OAuth2.OAuth2Service;
 import com.example.umc7th.domain.member.service.command.MemberCommandService;
 import com.example.umc7th.global.apiPayload.CustomResponse;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,15 +28,17 @@ public class MemberController {
         return CustomResponse.onSuccess(memberCommandService.signUp(dto));
     }
 
+    private final OAuth2Service oAuth2Service;
+
     //소셜 로그인
     @GetMapping("/oauth2/callback/kakao")
     // queryParam 형식으로 코드를 받을 예정이니 RequestParam을 설정해줍니다
     // 응답은 저희 서버에 로그인 다 한 뒤에 토큰을 제공할 예정이니 TokenDTO로 설정해줍니다.
     public CustomResponse<MemberResponseDTO.MemberTokenDTO> loginWithKakao(@RequestParam("code") String code) {
         // 로직 구현 필요
-        return null;
-        // 서비스 생성 이후
-        // return CustomResponse.onSuccess(oAuth2Service.login(code));
-
+        MemberResponseDTO.MemberTokenDTO tokenDTO =oAuth2Service.login(code);
+        return CustomResponse.onSuccess(tokenDTO);
     }
+
 }
+
