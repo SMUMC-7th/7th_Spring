b2571870794b6bfc6d6b0faaf29ab35525dc0f6c
Jeongmo Seo <wjdah1402@gmail.com>
2024-11-12 20:28:03 +0900

    :bug: fix: 7주차 수정

--- src/main/java/com/example/umc7th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc7th/domain/member/controller/MemberController.java
@@ -25,9 +25,8 @@ public class MemberController {
         return CustomResponse.onSuccess(memberCommandService.signUp(dto));
     }
 
-    @GetMapping("/oauth2/callback/{provider}")
-    public CustomResponse<MemberResponseDTO.MemberTokenDTO> loginWithKakao(@PathVariable String provider,
-                                                                           @RequestParam("code") String code) {
-        return CustomResponse.onSuccess(oAuth2Service.login(provider, code));
+    @GetMapping("/oauth2/callback/kakao")
+    public CustomResponse<MemberResponseDTO.MemberTokenDTO> loginWithKakao(@RequestParam("code") String code) {
+        return CustomResponse.onSuccess(oAuth2Service.login("kakao", code));
     }
 }
