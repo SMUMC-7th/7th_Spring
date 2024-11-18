f9d334d69f74e01d89b8ad1c3fb1824d9755a208
RCNR <iop04226@gmail.com>
2024-11-18 23:43:30 +0900

    :sparkles: feat: MemberErrorCode 추가

--- src/main/java/com/example/umc7th/domain/member/exception/MemberErrorCode.java
+++ src/main/java/com/example/umc7th/domain/member/exception/MemberErrorCode.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements BaseErrorCode {
 
     NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER404", "멤버를 찾지 못했습니다."),
+    ALREADY_EXIST(HttpStatus.BAD_REQUEST, "MEMBER400", "이미 존재하는 사용자입니다."),
+    INCORRECT_PASSWORD(HttpStatus.UNAUTHORIZED, "MEMBER401", "비밀번호가 틀립니다."),
     EXIST_EMAIL(HttpStatus.CONFLICT,
             "MEMBER409",
             "이미 사용중인 이메일입니다."),
